b5859b7a55c6a5c4dee7af3775af7e9abc9b344b
ritikxyz0 <legendxyz12352@gmail.com>
2025-05-12 00:29:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7763566676:AAFPUFSFHkdnPppDy5ov6tumAsWuTcgltUU")
+BOT_TOKEN=("7758619230:AAEY6G0uIanl_Hpn0_YegCJI7DE8h-M44cs")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@Mr_sid_001")
+OWNER_USERNAME=("@ritikxyz099")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1329951770")
+ADMIN_IDS=("6437994839")
 
 
 
